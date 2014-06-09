56133595dc84846d73c8fa813d849da40b8f74a2
iikun <djzielony@gmail.com>
2014-06-09 20:12:25 +0200

    Screensaver prevention in windows

--- main.cpp
+++ main.cpp
@@ -2,12 +2,27 @@
 #include <QApplication>
 
 #include "cockpit.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
 //Warthunder Cockpit "Rebirth"
 // By M. "iikun" D.
 //LGPL
 
 int main(int argc, char *argv[])
 {
+   #ifdef _WIN32
+    cout << "WIndows screensaver disable"<<endl;
+    SetThreadExecutionState(ES_CONTINUOUS|ES_DISPLAY_REQUIRED );
+   #elif defined __unix__
+   //do something for unix like #include <unistd.h>
+   #elif defined __APPLE__
+   //do something for mac
+   #endif
+
+
+
     QApplication a(argc, argv);
     Cockpit *window = new Cockpit;
 
